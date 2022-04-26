f997f15ff63cbe79e842fc6ffa943b9bdea21eba
Sergio Lopez <sergio@softnya.com>
2022-04-25 21:17:56 -0500

    Activacion de linter

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,13 +1,13 @@
-const ExplorerService = require('./../../lib/services/ExplorerService')
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
-describe('ExplorerService', () => {
-    test('should return a list of explorers in node', () => {
+describe("ExplorerService", () => {
+    test("should return a list of explorers in node", () => {
         const explorers = [
             {
-                mission: 'node'
+                mission: "node"
             }
-        ]
-        const ExplorersWithNode = ExplorerService.filterByMission(explorers, 'node')
-        expect(ExplorersWithNode.length).toBe(1)
-    })
-})
+        ];
+        const ExplorersWithNode = ExplorerService.filterByMission(explorers, "node");
+        expect(ExplorersWithNode.length).toBe(1);
+    });
+});
